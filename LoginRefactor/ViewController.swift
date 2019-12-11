7ca56090867dd5020d2b51c3d539440a75848106
meox92@gmail.com <meox92@gmail.com>
2019-12-11 21:22:25 +0100

    Initial Commit

---
+++ LoginRefactor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoginRefactor
+//
+//  Created by Maola Ma on 11/12/2019.
+//  Copyright © 2019 Maola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
